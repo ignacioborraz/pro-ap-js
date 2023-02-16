35c07c68d3881c5d59ae6ba37323de701adac52d
ignacioborraz <ignacioborraz@hotmail.com>
2023-02-16 13:52:19 -0300

    condicionales

---
+++ scripts/intro.js
@@ -0,0 +1,19 @@
+/**
+ * First question
+ */
+let preguntaNombre = 'cómo se llama su mascota?'
+let nombre = prompt(preguntaNombre)
+
+/**
+ * Second question
+ */
+let preguntaEdad = 'cuántos años tiene su mascota?'
+let edad = Number(prompt(preguntaEdad))
+
+/**
+ * Third question
+ */
+let preguntaMascota = 'qué mascota tiene? (perro-gato-loro-conejo-tortuga)'
+let mascota = prompt(preguntaMascota)
+
+console.log(nombre, edad, mascota)
