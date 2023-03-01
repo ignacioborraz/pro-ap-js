ed71a7cc6c1e2e3a8856c637606cb68c3c9fb9d5
ignacioborraz <ignacioborraz@hotmail.com>
2023-02-28 23:40:40 -0300

    hof

--- scripts/animals.js
+++ scripts/animals.js
@@ -1,8 +1,51 @@
 let animales = [
-    {nombre: 'gato', foto: 'animal1.png', patas: 4, edad_actual: 4, tipo: 'terrestre'},
-    {nombre: 'perro', foto: 'animal2.png', patas: 4, edad_actual: 2, tipo: 'terrestre'},
-    {nombre: 'tortuga', foto: 'animal3.png', patas: 4, edad_actual: 39, tipo: 'terrestre/acuatico'},
-    {nombre: 'conejo', foto: 'animal4.png', patas: 4, edad_actual: 5, tipo: 'terrestre'},
-    {nombre: 'loro', foto: 'animal5.png', patas: 2, edad_actual: 20, tipo: 'aereo'},
-    {nombre: 'tiburón', foto: 'animal6.png', patas: 0, edad_actual: 7, tipo: 'acuatico'}
+    {
+        nombre: 'gato',
+        foto: 'animal1.png',
+        patas: 4,
+        edad_actual: 4,
+        tipo: 'terrestre',
+        cantidad_mascotas: 5,
+        gasto_mensual_por_mascota: 2000,
+    },{
+        nombre: 'perro',
+        foto: 'animal2.png',
+        patas: 4,
+        edad_actual: 2,
+        tipo: 'terrestre',
+        cantidad_mascotas: 8,
+        gasto_mensual_por_mascota: 5000,
+    },{
+        nombre: 'tortuga',
+        foto: 'animal3.png',
+        patas: 4,
+        edad_actual: 39,
+        tipo: 'terrestre/acuatico',
+        cantidad_mascotas: 2,
+        gasto_mensual_por_mascota: 1500
+    },{
+        nombre: 'conejo',
+        foto: 'animal4.png',
+        patas: 4,
+        edad_actual: 5,
+        tipo: 'terrestre',
+        cantidad_mascotas: 29,
+        gasto_mensual_por_mascota: 1800
+    },{
+        nombre: 'loro',
+        foto: 'animal5.png',
+        patas: 2,
+        edad_actual: 20,
+        tipo: 'aereo',
+        cantidad_mascotas: 2,
+        gasto_mensual_por_mascota: 1200
+    },{
+        nombre: 'tiburón',
+        foto: 'animal6.png',
+        patas: 0,
+        edad_actual: 7,
+        tipo: 'acuatico',
+        cantidad_mascotas: 1,
+        gasto_mensual_por_mascota: 53000
+    }
 ]
--- scripts/hof.js
+++ scripts/hof.js
@@ -1,59 +1,91 @@
 /**
- * @map para transformar un array de objetos en un array de cards
+ * @map para transformar un array en otro array
+ * lo van a poder aplicar para transformar el array de eventos en un array de cards
  */
+const soloNombres = (each) => each.nombre //el parámetro each corresponde a un elemento del array a iterar
 //console.log(animales.map(soloNombres))
-function soloNombres(each) { //el parámetro each corresponde a un elemento del array a iterar
-    let nombre = each.nombre
-    return nombre
-}
-
+const nombreConParrafo = (each) => `<p>${each.nombre}</p>`
 //console.log(animales.map(nombreConParrafo))
-function nombreConParrafo(each) { //el parámetro each corresponde a un elemento del array a iterar
-    let nombre = each.nombre
-    let template = `<p>${nombre}</p>`
-    return template
-}
-
-//console.log(animales.map(defineTemplate))
-animales.map((each) => defineCard('20rem',each,3))
-//console.log(animales.map(each => each.edad_actual))
-let frases = animales.map(each => `el animal ${each.nombre} tiene ${each.edad_actual} años`)
-//animales = animales.map(each => `el animal ${each.nombre} tiene ${each.edad_actual} años`)
+//console.log(animales.map(defineTemplate)) //funcion con el template ya definido
+//let frases = animales.map(each => `el animal ${each.nombre} tiene ${each.edad_actual} años`)
 //console.log(frases)
 //console.log(animales)
 
 /**
- * @foreach ??? (definir tipos sin repetir)
+ * @foreach ??? para muchas cosas o ninguna (en este ejemplo para definir tipos sin repetir)
+ * lo van a poder aplicar para definir dinamicamente todas las categorias de eventos (sin repetir)
  */
-console.log(animales.forEach(each => each.edad_actual))
+//console.log(animales.forEach(each => each.edad_actual)) //no retorna ni transforma nada
 let tipos = [] //tipos va a ser el array que va a tener los tipos de animal SIN repetirse
-console.log(animales.forEach(each => {
+animales.forEach(each => {
     if ( ! tipos.includes(each.tipo) ) {
         tipos.push(each.tipo)
     }    
-}))
-console.log(tipos)
+})
+//console.log(tipos)
 
 /**
- * @find para encontrar el primer animal que cumpla una condicion (nombre => id)
+ * @find para encontrar el primer elemento que cumpla una condicion (booleano)
+ * lo van a poder aplicar para encontrar el evento de la página de detalle
  */
-console.log(animales.find(each => each.tipo === "terrestre"))
-console.log(animales.find(each => each.edad_actual > 18))
+let terrestre = animales.find(each => each.tipo === "terrestre")
+//console.log(terrestres)
+let mayorDeEdad = animales.find(each => each.edad_actual > 18)
+//console.log(mayorDeEdad)
 
 /**
- * @filter para encontrar todos los animales que cumplan una condicion (tipo)
+ * @filter para encontrar todos los elementos que cumplan una condición (booleano)
+ * lo van a poder aplicar para los filtros
  */
-console.log(animales.filter(each => each.tipo === "terrestre"))
-console.log(animales.filter(each => each.patas >= 2))
+let terrestres = animales.filter(each => each.tipo === "terrestre")
+//console.log(terrestres)
+let masDeDosPatas = animales.filter(each => each.patas >= 2)
+//console.log(masDeDosPatas)
 
 /**
- * @sort para ordenar por nombre
+ * @sort para ordenar
+ * [...array] define una copia de ese aray
+ * lo van a poder aplicar para ordenar los eventos y tambien para ordenar los datos de la tabla dinamica (tambien obtener min/max)
  */
+let sorted1 = [...animales].sort((elemento1,elemento2)=>elemento1.edad_actual-elemento2.edad_actual) //de menor a mayor con el MENOS
+//console.log(sorted1)
+//console.log('animal con mayor edad '+sorted1[sorted1.length-1].nombre+' con '+sorted1[sorted1.length-1].edad_actual)
+//console.log(`animal con menor edad ${sorted1[0].nombre} con ${sorted1[0].edad_actual}`)
+let sorted2 = [...animales].sort((e1,e2)=>e1.patas+e2.patas) //de mayor a menor con el MAS
+//console.log(sorted2)
+//console.log('animal con mayor cantidad de patas '+sorted2[0].nombre)
+//console.log('animal con menor cantidad de patas '+sorted2[sorted2.length-1].nombre)
 
 /**
- * @sort para min-max de edad
+ * @reduce para operar un array y realizar operaciones "de reduccion/acumulacion"
  */
+let gasto_total = animales.reduce(
+    (acc,each)=> { //acc es el acumulador (alcancia), each son los datos de cada uno de los elementos del array
+        //console.log(acc)
+        //console.log(each)
+        let cantidad = each.cantidad_mascotas
+        let gasto = each.gasto_mensual_por_mascota
+        let gasto_total = cantidad*gasto
+        return acc + gasto_total
+    },
+    //y entre llaves programo la logica necesaria para reducir los datos
+    0
+)
+//console.log(gasto_total)
 
-/**
- * @reduce para sacar el promedio de gasto anual de las mascotas
- */
+let datos = animales.reduce(
+    (acc,each)=>{
+        let cantidad = each.cantidad_mascotas //de cada elemento, la cantidad de mascotas
+        let gasto = each.gasto_mensual_por_mascota //de cada elemento, el gasto de cada mascota
+        let obj_acc = {} //objeto que va a acumular todo
+        obj_acc = { //acumulo
+            suma_gastos: acc.suma_gastos + gasto*cantidad, //a lo que ya habia, le sumo lo nuevo
+            cantidad_animales: acc.cantidad_animales + cantidad, //a lo que ya habia, le sumo lo nuevo
+        }
+        obj_acc.promedio = obj_acc.suma_gastos/obj_acc.cantidad_animales //defino el promedio
+        return obj_acc //retorno el objeto acumulador
+    },
+    //0 necesito acumular dos variables por lo que es más comodo que el valor inicial de la reduccion sea un objeto
+    { suma_gastos: 0, cantidad_animales: 0, promedio: 0 } //tengo que acumular más de un dato, por eso es apropiado iniciar la reduccion con un objeto
+)
+//console.log(datos)
